91c13850aca93816d360f2bf09d6781c16db5bb5
fariassilva <alessandrofarias405@gmail.com>
2023-02-17 14:08:03 -0300

    Tema e inputs

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
-import { createGlobalStyle  } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+
+import theme from '../src/theme'
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -8,16 +10,29 @@ const GlobalStyle = createGlobalStyle`
 
 body {
   font-family: 'Roboto', sans-serif;
+  color: ${props => props.theme.black};
+  background-color: ${props => props.theme.background};
 }
+a{
+
+  color: ${props => props.theme.primary};
+  font-weight: bold;
+  text-decoration: none;
+  transition: all 0.3s;
+
+}
+a:houver {
+  color: ${props => props.theme.primaryHouver};
+}
+
 `
 
 function App ({ Component, pageProps }) {
   return (
-    <>
-    <GlobalStyle/>
+    <ThemeProvider theme={theme}>
+     <GlobalStyle/>
      <Component {...pageProps} />
-    
-    </>
+    </ThemeProvider>
    
   )
 }
